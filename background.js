e4916a816ca61e164670bb081e6665581a68df59
unknown <t-urosm@microsoft.com>
2023-08-06 00:31:00 +0200

    prvi pokusaj priklupljana elemanata sa chess.com-a

---
+++ background.js
@@ -0,0 +1,13 @@
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  console.log("KURWAMA")
+    if (tab.url && tab.url.includes("chess.com")) {
+      console.log(tab.url);
+      console.log("KURAC ")
+      chrome.tabs.sendMessage(tabId, {
+        type: "NEW",
+        videoId: "kurcina",
+      });
+    }
+  });
+  
